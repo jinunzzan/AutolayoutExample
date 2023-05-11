d4856f1215586bd775f662d7cf2c7526a0c0c0ed
jinunzzan <lovebox6607@gmail.com>
2023-05-11 14:36:44 +0900

    아울렛변수, 액션함수 사용해보기

--- AutolayoutExample/ViewController.swift
+++ AutolayoutExample/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    //struc 으로 선언하면 메모리 누수가 발생될수있음
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+//색상변경하기
+    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = .blue
+        print("색상변경 버튼이 클릭되었음")
+    }
 }
 
